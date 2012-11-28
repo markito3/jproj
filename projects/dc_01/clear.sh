bbfd122e0a16559ee2046c5febd892e4bae1b170
Mark Ito <marki@jlab.org>
2012-11-28 23:44:27 +0000

    10000 jobs

--- projects/dc_01/clear.sh
+++ projects/dc_01/clear.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 # delete the project and start over
 mysql -hhalldweb1 -ufarmer farming -e "drop table dc_01"
-../../scripts/jproj.pl dc_01 create 1000
+../../scripts/jproj.pl dc_01 create 10000
