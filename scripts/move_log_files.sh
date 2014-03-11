7a206362b261612c203b940856c03efc46423c5a
Mark Ito <marki@jlab.org>
2014-03-11 18:31:04 +0000

    put jsub and script file into a subdirectory of /scratch/user

--- scripts/move_log_files.sh
+++ scripts/move_log_files.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 job_id=$1
 target_directory=$2
+mkdir -p /u/scratch/$USER/move
 # create command file
-command_file=/u/scratch/$USER/move_${job_id}.sh
+command_file=/u/scratch/$USER/move/move_${job_id}.sh
 touch $command_file
 echo \#\!/bin/sh >> $command_file
 echo echo sleep 200 >> $command_file
@@ -13,7 +14,7 @@ echo echo mv -v $HOME/.farm_out/\*$job_id\* $target_directory >> $command_file
 echo mv -v $HOME/.farm_out/\*$job_id\* $target_directory >> $command_file
 chmod a+x $command_file
 # create jsub file
-jsub_file=/u/scratch/$USER/move_${job_id}.jsub
+jsub_file=/u/scratch/$USER/move/move_${job_id}.jsub
 touch $jsub_file
 echo PROJECT: gluex >> $jsub_file
 echo JOBNAME: move_$job_id >> $jsub_file
