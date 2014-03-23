039db70d1f69015e9198b29edacc6d83c2d3bc52
Mark Ito <marki@jlab.org>
2014-03-23 23:45:44 +0000

    works

--- projects/test/clear.sh
+++ projects/test/clear.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 # delete the project and start over
-mysql -hhalldweb1 -ufarmer farming -e "drop table test"
-../../scripts/jproj.pl dc_02 create 1234 1000
+mysql -hhalldweb1 -ufarmer farming -e "drop table test; drop table testJob"
+../../scripts/jproj.pl test create 1234 10
