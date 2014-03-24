85d99d8eff968de88de15fb0be342e19dd9451aa
Mark Ito <marki@jlab.org>
2014-03-24 23:31:35 +0000

    go back to the debug track

--- scripts/move_log_files.sh
+++ scripts/move_log_files.sh
@@ -18,7 +18,7 @@ jsub_file=/u/scratch/$USER/move/move_${job_id}.jsub
 touch $jsub_file
 echo PROJECT: gluex >> $jsub_file
 echo JOBNAME: move_$job_id >> $jsub_file
-echo TRACK: simulation >> $jsub_file
+echo TRACK: debug >> $jsub_file
 echo COMMAND: $command_file >> $jsub_file
 echo MEMORY: 500 MB >> $jsub_file
 jsub $jsub_file
