a219844585ded1007f490c1623dafdb04c50998c
Mark Ito <marki@jlab.org>
2014-12-18 21:09:01 +0000

    changes

--- projects/dc_03_sim/clear.sh
+++ projects/dc_03_sim/clear.sh
@@ -1,5 +1,4 @@
 #/bin/sh
 # delete the project and start over
-mysql -hhalldweb1 -ufarmer farming -e "drop table dc_03_sim"
-../../scripts/jproj.pl dc_03_sim create
-../../scripts/jproj.pl dc_03_sim update
+mysql -hhalldweb1 -ufarmer farming -e "drop table dc_03_sim, dc_03_simJob"
+../../scripts/jproj.pl dc_03_sim create 9200 10
