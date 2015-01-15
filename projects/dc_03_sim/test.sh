8859f55ae9ce7ffd4ec74d3666e4a18948f9c062
Mark Ito <marki@jlab.org>
2015-01-15 23:07:53 +0000

    get proc id right

--- projects/dc_03_sim/test.sh
+++ projects/dc_03_sim/test.sh
@@ -2,8 +2,9 @@
 project=dc_03_sim
 run=9100
 file=1
-rundir=/scratch/$USER/test.$!
+rundir=/u/scratch/$USER/test.$$
 script=/home/$USER/halld/jproj/projects/$project/$project.csh
 mkdir -p $rundir
 cd $rundir
 $script $project $run $file
+exit
