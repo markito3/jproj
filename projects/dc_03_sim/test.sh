eda8036ce675094f1a82b2ebe3a11cc90e3de5f5
Mark Ito <marki@jlab.org>
2015-01-16 00:26:21 +0000

    use work disk

--- projects/dc_03_sim/test.sh
+++ projects/dc_03_sim/test.sh
@@ -2,7 +2,7 @@
 project=dc_03_sim
 run=9100
 file=1
-rundir=/u/scratch/$USER/test.$$
+rundir=/work/halld/home/$USER/test.$$
 script=/home/$USER/halld/jproj/projects/$project/$project.csh
 mkdir -p $rundir
 cd $rundir
