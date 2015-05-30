cdadb9c0e735115ff7f5d158475840d8d0b7c796
Mark Ito <marki@jlab.org>
2015-05-30 20:18:59 +0000

    helper scripts

---
+++ projects/detcom_02/populate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+njobs=1000
+jproj.pl detcom_02 populate 9301 $njobs
+jproj.pl detcom_02 populate 9305 $njobs
+jproj.pl detcom_02 populate 9306 $njobs
+jproj.pl detcom_02 populate 9311 $njobs
+jproj.pl detcom_02 populate 9315 $njobs
+jproj.pl detcom_02 populate 9316 $njobs
