4e1fe48ceefb87a18ce12cc847f49529d6bcd2ea
Mark Ito <marki@jlab.org>
2015-06-02 14:21:56 +0000

    use populate action

--- projects/dc_03_sim/clear.sh
+++ projects/dc_03_sim/clear.sh
@@ -1,4 +1,5 @@
-#/bin/sh
+#!/bin/sh
 # delete the project and start over
 mysql -hhallddb -ufarmer farming -e "drop table dc_03_sim, dc_03_simJob"
-../../scripts/jproj.pl dc_03_sim create 9200 10000
+jproj.pl dc_03_sim create
+jproj.pl dc_03_sim populate 9200 10
