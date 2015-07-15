22a39364ca0c6142539cda2de391a5575f18cf1a
Mark Ito <marki@jlab.org>
2015-07-15 20:02:10 +0000

    go to 10 jobs initially

--- projects/detcom_02/clear.sh
+++ projects/detcom_02/clear.sh
@@ -2,15 +2,15 @@
 # delete the project and start over
 mysql -hhallddb -ufarmer farming -e "drop table detcom_02; drop table detcom_02Job"
 jproj.pl detcom_02 create
-jproj.pl detcom_02 populate 9301 1
-jproj.pl detcom_02 populate 9302 1
-jproj.pl detcom_02 populate 9303 1
-jproj.pl detcom_02 populate 9304 1
-jproj.pl detcom_02 populate 9305 1
-jproj.pl detcom_02 populate 9306 1
-jproj.pl detcom_02 populate 9311 1
-jproj.pl detcom_02 populate 9312 1
-jproj.pl detcom_02 populate 9313 1
-jproj.pl detcom_02 populate 9314 1
-jproj.pl detcom_02 populate 9315 1
-jproj.pl detcom_02 populate 9316 1
+jproj.pl detcom_02 populate 9301 10
+jproj.pl detcom_02 populate 9302 10
+jproj.pl detcom_02 populate 9303 10
+jproj.pl detcom_02 populate 9304 10
+jproj.pl detcom_02 populate 9305 10
+jproj.pl detcom_02 populate 9306 10
+jproj.pl detcom_02 populate 9311 10
+jproj.pl detcom_02 populate 9312 10
+jproj.pl detcom_02 populate 9313 10
+jproj.pl detcom_02 populate 9314 10
+jproj.pl detcom_02 populate 9315 10
+jproj.pl detcom_02 populate 9316 10
