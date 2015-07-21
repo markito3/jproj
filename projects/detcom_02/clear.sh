a4b12fca2bdb8e2f9b4001f2974a173ea5ab476e
Mark Ito <marki@jlab.org>
2015-07-21 20:45:30 +0000

    populate the whole project

--- projects/detcom_02/clear.sh
+++ projects/detcom_02/clear.sh
@@ -2,6 +2,7 @@
 # delete the project and start over
 mysql -hhallddb -ufarmer farming -e "drop table detcom_02; drop table detcom_02Job"
 jproj.pl detcom_02 create
+
 jproj.pl detcom_02 populate 9301 10
 jproj.pl detcom_02 populate 9302 10
 jproj.pl detcom_02 populate 9303 10
@@ -14,3 +15,51 @@ jproj.pl detcom_02 populate 9313 10
 jproj.pl detcom_02 populate 9314 10
 jproj.pl detcom_02 populate 9315 10
 jproj.pl detcom_02 populate 9316 10
+
+jproj.pl detcom_02 populate 9301 90
+jproj.pl detcom_02 populate 9302 90
+jproj.pl detcom_02 populate 9303 90
+jproj.pl detcom_02 populate 9304 90
+jproj.pl detcom_02 populate 9305 90
+jproj.pl detcom_02 populate 9306 90
+jproj.pl detcom_02 populate 9311 90
+jproj.pl detcom_02 populate 9312 90
+jproj.pl detcom_02 populate 9313 90
+jproj.pl detcom_02 populate 9314 90
+jproj.pl detcom_02 populate 9315 90
+jproj.pl detcom_02 populate 9316 90
+
+jproj.pl detcom_02 populate 9301 900
+jproj.pl detcom_02 populate 9305 900
+jproj.pl detcom_02 populate 9306 900
+jproj.pl detcom_02 populate 9311 900
+jproj.pl detcom_02 populate 9315 900
+jproj.pl detcom_02 populate 9316 900
+
+jproj.pl detcom_02 populate 9301 1000
+jproj.pl detcom_02 populate 9305 1000
+jproj.pl detcom_02 populate 9306 1000
+jproj.pl detcom_02 populate 9311 1000
+jproj.pl detcom_02 populate 9315 1000
+jproj.pl detcom_02 populate 9316 1000
+
+jproj.pl detcom_02 populate 9301 1000
+jproj.pl detcom_02 populate 9305 1000
+jproj.pl detcom_02 populate 9306 1000
+jproj.pl detcom_02 populate 9311 1000
+jproj.pl detcom_02 populate 9315 1000
+jproj.pl detcom_02 populate 9316 1000
+
+jproj.pl detcom_02 populate 9301 1000
+jproj.pl detcom_02 populate 9305 1000
+jproj.pl detcom_02 populate 9306 1000
+jproj.pl detcom_02 populate 9311 1000
+jproj.pl detcom_02 populate 9315 1000
+jproj.pl detcom_02 populate 9316 1000
+
+jproj.pl detcom_02 populate 9301 1000
+jproj.pl detcom_02 populate 9305 1000
+jproj.pl detcom_02 populate 9306 1000
+jproj.pl detcom_02 populate 9311 1000
+jproj.pl detcom_02 populate 9315 1000
+jproj.pl detcom_02 populate 9316 1000
