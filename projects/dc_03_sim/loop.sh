dc2f4d0c17d6ff745be383381ba138b9613aec48
Mark Ito <marki@jlab.org>
2015-08-18 16:13:23 +0000

    loop through the jproj steps

---
+++ projects/dc_03_sim/loop.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+fill_in_job_details.pl dc_03_sim
+jproj.pl dc_03_sim update_output /volatile/halld/data_challenge/dc_03_sim/smeared
+jproj.pl dc_03_sim jput /volatile/halld/data_challenge/dc_03_sim/smeared /mss/halld/halld-scratch/data_challenge/dc_03_sim/smeared4
+jproj.pl dc_03_sim update_silo /mss/halld/halld-scratch/data_challenge/dc_03_sim/smeared4
