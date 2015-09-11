046a205cf1b89e377aa812c09e9a1418e2ce0587
Mark Ito <marki@jlab.org>
2015-09-11 15:57:56 +0000

    add submissions

--- projects/dc_03_sim/loop.sh
+++ projects/dc_03_sim/loop.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+jproj.pl dc_03_sim submit 200
 fill_in_job_details.pl dc_03_sim
 jproj.pl dc_03_sim update_output /volatile/halld/data_challenge/dc_03_sim/smeared
 jproj.pl dc_03_sim jput /volatile/halld/data_challenge/dc_03_sim/smeared /mss/halld/halld-scratch/data_challenge/dc_03_sim/smeared4
