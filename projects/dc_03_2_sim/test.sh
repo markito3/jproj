5799c6704ab294d5c6e63e8516d7d810a22d4c85
GlueX-Hall-D (Mark Ito) <gxproj2@ifarm1102.jlab.org>
2015-10-27 08:38:03 -0400

    Run the test on the scratch disk.

--- projects/dc_03_2_sim/test.sh
+++ projects/dc_03_2_sim/test.sh
@@ -2,8 +2,8 @@
 project=dc_03_2_sim
 run=9100
 file=1
-rundir=/work/halld/home/$USER/test.$$
-script=/home/$USER/halld/jproj/projects/$project/$project.csh
+rundir=/u/scratch/$USER/test.$$
+script=`pwd`/$project.csh
 mkdir -p $rundir
 cd $rundir
 $script $project $run $file
