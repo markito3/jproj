727fcc9adc1207dbc79d2add9d74f775e7622424
GlueX-Hall-D (Mark Ito) <gxproj2@ifarm1401.jlab.org>
2015-10-27 14:13:38 -0400

    Add deletion of swif workflow.

--- projects/dc_03_2_sim/clear.sh
+++ projects/dc_03_2_sim/clear.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # delete the project and start over
+swif cancel -delete -workflow dc_03_2_sim
 mysql -hhallddb -ufarmer farming -e "drop table dc_03_2_sim, dc_03_2_simJob"
 jproj.pl dc_03_2_sim create
 jproj.pl dc_03_2_sim populate 9200 10
