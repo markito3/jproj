139999409bb00b8c2bc49e002264fbcc9d51f6ee
Mark Ito <marki@jlab.org>
2015-11-12 14:02:04 -0500

    Add deletion of swif workflow.

--- projects/sim1/clear.sh
+++ projects/sim1/clear.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # delete the project and start over
+swif cancel -workflow sim1 -delete
 mysql -hhallddb -ufarmer farming2 -e "drop table sim1; drop table sim1Job"
 jproj.pl sim1 create
 jproj.pl sim1 populate 9001 10
