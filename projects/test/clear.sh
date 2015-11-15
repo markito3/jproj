409095c95fbe9849ebbba0e2dff5c0083922d201
Mark Ito <marki@jlab.org>
2015-11-15 18:36:58 -0500

    change from farming to farming2

--- projects/test/clear.sh
+++ projects/test/clear.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 # delete the project and start over
 swif cancel test -delete
-mysql -hhallddb -ufarmer farming -e "drop table test; drop table testJob"
+mysql -hhallddb -ufarmer farming2 -e "drop table test; drop table testJob"
 jproj.pl test create
 jproj.pl test populate 1234 10
