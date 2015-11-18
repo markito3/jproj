79e6aebf6bebc9020d460957ad84209f47ed017c
markito3 <marki@jlab.org>
2015-11-18 08:17:28 -0500

    add swif cancel -delete

--- projects/dc_03_2_recon/clear.sh
+++ projects/dc_03_2_recon/clear.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # delete the project and start over
+swif cancel -delete -workflow dc_03_2_recon
 mysql -hhallddb -ufarmer farming -e "drop table dc_03_2_recon, dc_03_2_reconJob"
 jproj.pl dc_03_2_recon create
 jproj.pl dc_03_2_recon update
