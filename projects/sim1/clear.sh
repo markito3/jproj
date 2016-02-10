19df0581c0baf0060b4a2b23378e67ac44ca19be
Mark Ito <marki@jlab.org>
2016-02-09 21:36:56 -0500

    change to run 10000

--- projects/sim1/clear.sh
+++ projects/sim1/clear.sh
@@ -3,4 +3,4 @@
 swif cancel -workflow sim1 -delete
 mysql -hhallddb -ufarmer farming2 -e "drop table sim1; drop table sim1Job"
 jproj.pl sim1 create
-jproj.pl sim1 populate 9001 10
+jproj.pl sim1 populate 10000 1000
