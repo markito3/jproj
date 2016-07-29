aea7631c19e5ae990bc42a96ce77914f1dacb255
Mark Ito <marki@jlab.org>
2016-07-29 10:54:15 -0400

    squelch type out of pointer types

--- scripts/jproj.pl
+++ scripts/jproj.pl
@@ -607,31 +607,34 @@ sub read_project_parameters {
 
 sub update_auger {
     $temp_file = "/tmp/jproj_${project}_auger.xml";
-    system("rm -f $temp_file");
+    system("rm -fv $temp_file");
     system("swif status -workflow=$project -jobs -display=xml > $temp_file");
 #    open the file and parse it
 
-    $debug_xml = 0;
+    my $debug_xml = 0;
     # slurp in the xml file
     my $ref = XMLin($temp_file, KeyAttr=>[], ForceArray => ['attempt']);
     # dump it to the screen for debugging only
-    if ($debug_xml) {print Dumper($ref);}
-
+    if ($debug_xml) {
+	print "====== begin dump =========\n";
+	print Dumper($ref);
+	print "====== end dump =========\n";
+    }
 #    add each entry indiscriminantly and give up if an error occurs because auger id should be a primary key
     my $jobs = $ref->{jobs}; # hash reference
     my $job = $jobs->{job}; # array reference
-    if ($debug_xml) {print "jobs = ", $jobs, " job = ", $job, "\n";}
     my @jobarray = @$job; # array
+    if ($debug_xml) {my $l = $#jobarray + 1; print "njobs = ", $l, "\n";}
     $jobtable = $project . "Job";
     for ($i = 0; $i <= $#jobarray; $i++) {
 	%thisjobhash = %{$jobarray[$i]};
 	$attempts = $thisjobhash{attempts};
 	$attempt = $attempts->{attempt};
 	my $jobid = $thisjobhash{id};
-	if ($debug_xml) {print "jobarray[$i] = ", $jobarray[$i], " thisjobhash{name} = ", $thisjobhash{name}, " jobid = ", $jobid, " attempts = ", $attempts, " attempt = ", $attempt, "\n";}
+	if ($debug_xml) {print " thisjobhash{name} = ", $thisjobhash{name}, " jobid = ", $jobid, "\n";}
 	foreach $this_attempt_element (@$attempt) {
 	    $augerid = $this_attempt_element->{auger_id};
-	    if ($debug_xml) {print "this_attempt_element = ", $this_attempt_element, " augerid = ", $augerid, "\n";}
+	    if ($debug_xml) {print " attempt augerid = ", $augerid, "\n";}
 	    $sql = "SELECT count(*) from $jobtable where augerId = $augerid;";
 	    make_query($dbh_db, \$sth);
 	    my $count = $sth->fetchrow_array();
