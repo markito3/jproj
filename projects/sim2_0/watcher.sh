142f562b434d65162bc891a6790543dc5fc827b2
Mark Ito <marki@jlab.org>
2017-11-09 11:19:34 -0500

    add sim 2.0

---
+++ projects/sim2_0/watcher.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+target=$1
+maxjobs=$2
+echo target = $target
+if [ "X$maxjobs" = X ]
+    then
+    maxjobs=10000
+fi
+echo maxjobs = $maxjobs
+rm -f watcher.tmp
+swif status -workflow sim2_0 > watcher.tmp
+dispatched=`grep dispatched watcher.tmp | grep -v undispatched | awk '{print $3}'`
+echo dispatched = $dispatched
+undispatched=`grep undispatched watcher.tmp | awk '{print $3}'`
+if [ "X$undispatched" = X ]
+    then
+    undispatched=0
+fi
+echo undispatched = $undispatched
+pending=`grep auger_pending watcher.tmp | awk '{print $3}'`
+if [ "X$pending" = X ]
+    then
+    pending=0
+fi
+echo pending = $pending
+deficit=$(($target-$dispatched-$undispatched))
+echo deficit = $deficit
+if [ "$deficit" -gt 0 -a "$pending" -eq 0 ]
+    then
+    if [ "$deficit" -gt "$maxjobs" ]
+	then
+	addjobs=$maxjobs
+    else
+	addjobs=$deficit
+    fi
+    echo add $addjobs job\(s\)
+    jproj.pl sim2_0 add $addjobs
+else
+    echo no need for more
+fi
